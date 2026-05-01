525b9290de52e24cbfdd053952cb813bc5841e29
Aymerick Tilly <aymerickbora@hotmail.fr>
2026-05-01 18:51:03 +1200

    Get one booking test

--- tests/booking.spec.ts
+++ tests/booking.spec.ts
@@ -11,3 +11,19 @@ test('GET /booking - should return a list of bookings', async ({ request }) => {
     expect(Array.isArray(body)).toBeTruthy();
     expect(body.length).toBeGreaterThan(0);
 });
+
+test('GET /booking/{id} - should return booking details', async ({ request }) => {
+
+    const response = await request.get(`${BASE_URL}/booking/1`);
+
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+    expect(body).toHaveProperty('firstname');
+    expect(body).toHaveProperty('lastname');
+    expect(body).toHaveProperty('totalprice');
+    expect(body).toHaveProperty('depositpaid');
+    expect(body).toHaveProperty('bookingdates');
+    expect(body.bookingdates).toHaveProperty('checkin');
+    expect(body.bookingdates).toHaveProperty('checkout');
+});
